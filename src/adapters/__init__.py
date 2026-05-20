a9c2b3804c0bd118c2dc5052c0489f65c3eecdb4
Axel RG <rg_axel@yahoo.fr>
2026-05-20 19:57:46 +0200

    rename gatewe an adapters et suppression de l'ancien dossier adapters

--- src/adapters/__init__.py
+++ src/adapters/__init__.py
@@ -1,3 +1,11 @@
-from adapters.console_adapter import afficher_liste_console, afficher_tome_console
+from adapters.file_system import FileSystemAdapter
+from adapters.html_reader import HTMLReaderAdapter
+from adapters.logger import LoggerAdapter
+from adapters.tome_file_repository import TomeFileRepository
 
-__all__ = ["afficher_liste_console", "afficher_tome_console"]
+__all__ = [
+    "TomeFileRepository",
+    "LoggerAdapter",
+    "FileSystemAdapter",
+    "HTMLReaderAdapter",
+]
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-from gateways import (
+from adapters import (
     FileSystemAdapter,
     HTMLReaderAdapter,
     LoggerAdapter,
