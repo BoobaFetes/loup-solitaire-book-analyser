bf49bc8a7efdae39606b2aa9e0cbe51cf304bf4c
Axel RG <rg_axel@yahoo.fr>
2026-05-27 17:09:35 +0200

    deliver version 1.0.4 manually

--- scripts/new-docker-image.ps1
+++ scripts/new-docker-image.ps1
@@ -1,14 +1,13 @@
 #arguments
 param(
     [string]$image = "loup-solitaire-book-analyser",
-    [string]$tag = "v1.0.3"
+    [string]$tag = "v1.0.4"
 )
 $rootPath = "$psscriptroot/.."
-$tags = @($tag, "latest")
-foreach ($t in $tags) {
-    $imageTag = "$($image):$t"
-    docker build -t $imageTag -f "$rootPath/Dockerfile" $rootPath
-    write-host "Image $imageTag built successfully."
-}
+
+$imageTag = "$($image):$t"
+docker build -t $imageTag -f "$rootPath/Dockerfile" $rootPath
+write-host "Image $imageTag built successfully."
+
 
 docker image ls --filter "reference=$image"
