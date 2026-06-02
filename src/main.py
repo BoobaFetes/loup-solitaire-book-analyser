fbee100557de39e6714ee4a4511606dc05a541b9
Axel RG <rg_axel@yahoo.fr>
2026-06-02 21:04:45 +0200

    ajout d'un main.py pour permettre de verifier que les applications sont bien configurées

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+root_path = Path(os.getcwd())
+
+log_file = root_path / "logs" / "test.log"
+data_file = root_path / "data" / "test.json"
+
+print("le pod est lancé !")
+
+print("tentative d'ecriture dans les volumes montés...")
+with open(log_file, "w") as f:
+    f.write("test d'écriture dans le volume monté")
+print("écriture terminée !")
+with open(data_file, "w") as f:
+    f.write("test d'écriture dans le volume monté")
+print("écriture terminée !")
+
+print("suppression des fichiers de test...")
+log_file.unlink()
+data_file.unlink()
+print("fichiers de test supprimés !")
+
+print("le pod se termine !")
