b73f7aa649f60b7ba16a0cf7faccb0d83b1aa275
Axel RG <rg_axel@yahoo.fr>
2026-07-02 18:14:04 +0200

    remove useless scrip clear-worknode-data.ps1

--- scripts/clear-workernode-data.ps1
+++
@@ -1,8 +0,0 @@
-write-host "files before:"
-docker exec -it desktop-worker sh -c "ls ./mnt/lsba/**/**/*"
-
-write-host "deleting files..."
-docker exec -it desktop-worker sh -c "rm ./mnt/lsba/**/**/*"
-
-write-host "files after:"
-docker exec -it desktop-worker sh -c "ls ./mnt/lsba/**/**/*"
