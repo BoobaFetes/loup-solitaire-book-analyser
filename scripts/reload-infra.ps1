6e7afb538f9da2274e160075861160054bee2129
Axel RG <rg_axel@yahoo.fr>
2026-08-18 17:41:06 +0200

    correctio,n des scripts de deploiement

--- scripts/reload-infra.ps1
+++ scripts/reload-infra.ps1
@@ -5,7 +5,7 @@ $scriptRoot = $PSScriptRoot
 
 
 write-host "Deleting the local build configuration..." -ForegroundColor Green
-k delete -f "$scriptRoot/../k8s/overlays/dev-build.yaml"
+kubectl delete -f "$scriptRoot/../k8s/overlays/dev-build.yaml"
 
 write-host "Deleting the local configuration..." -ForegroundColor Green
 & "$scriptRoot/clean-local-configuration.ps1"
@@ -17,10 +17,10 @@ if (!$Clean.IsPresent) {
     & "$scriptRoot/set-local-configuration.ps1"
 
     write-host "Building the local build configuration..." -ForegroundColor Green
-    kustomize build .\k8s\overlays\dev\ --enable-helm > "$scriptRoot/../k8s/overlays/dev-build.yaml"
+    kustomize build "$scriptRoot/../k8s/overlays/dev" --enable-helm > "$scriptRoot/../k8s/overlays/dev-build.yaml"
 
     write-host "Recreating the local build configuration..." -ForegroundColor Green
-    k apply -f "$scriptRoot/../k8s/overlays/dev-build.yaml"  
+    kubectl apply -f "$scriptRoot/../k8s/overlays/dev-build.yaml"
 
     write-host "Port forwarding the database on localhost..." -ForegroundColor Green
     start-sleep -Seconds 10
